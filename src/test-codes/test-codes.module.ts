789f4b2bcda58adfed9bb46d8b3b8c30c914734b
Iftekhairul <iftekhairul.islam@hotmail.com>
2026-03-06 01:19:45 +1100

    fix: ci-cd auto seeding off from test-codes module for safe deployment test

--- src/test-codes/test-codes.module.ts
+++ src/test-codes/test-codes.module.ts
@@ -1,4 +1,4 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TestCodesController } from './test-codes.controller';
 import { TestCodesService } from './test-codes.service';
@@ -11,10 +11,6 @@ import { TestCodesSeeder } from './seeder/test-codes.seeder';
   providers: [TestCodesService, TestCodesSeeder],
   exports: [TestCodesService],
 })
-export class TestCodesModule implements OnModuleInit {
-  constructor(private readonly testCodesSeeder: TestCodesSeeder) {}
-
-  async onModuleInit() {
-    await this.testCodesSeeder.seed();
-  }
+export class TestCodesModule {
+  constructor() {}
 }
