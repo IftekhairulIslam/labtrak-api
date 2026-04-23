6385f7f34e2ca1d027d6fad7f5ad5278f4e9d0f4
Iftekhairul <iftekhairul.islam@hotmail.com>
2026-04-24 08:53:03 +1000

    fix: reverting back to check from from ENV file

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:5173',
-    origin: ['http://localhost:5173', 'https://netlify.app/'],
+    origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
