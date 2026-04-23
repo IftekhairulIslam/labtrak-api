b467b157f5bb45770332419d5256962cff3153b7
Iftekhairul <iftekhairul.islam@hotmail.com>
2026-04-23 13:41:26 +1000

    fix: manual cors fix for netlify

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors({
     // origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:5173',
-    origin: ['http://localhost:5173', 'https://labtrak.netlify.app/'],
+    origin: ['http://localhost:5173', 'https://netlify.app/'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
